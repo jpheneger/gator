047fd2324a05418dc2b1cde65119945fa2555b19
jpheneger <jpheneger@gmail.com>
2025-06-04 11:43:40 -0700

    Add help command with usage examples

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) 
 		}
 		err = handler(s, cmd, user)
 		if err != nil {
-			return fmt.Errorf("unable to run command '%s' for user '%s'", cmd.Name, user.Name)
+			return fmt.Errorf("unable to run command '%s' for user '%s': %w", cmd.Name, user.Name, err)
 		}
 		return nil
 	}
@@ -51,6 +51,7 @@ func main() {
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
+	cmds.register("help", handlerHelp)
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
@@ -75,4 +76,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handlerHelp(s *state, cmd command) error {
+	fmt.Printf("login: Log in the currently active user.\nUsage: login <name>\n\n")
+	fmt.Printf("register: Register a new user.\nUsage: register <name>\n\n")
+	fmt.Printf("reset: Clear/empty the database.\nUsage: reset\n\n")
+	fmt.Printf("users: List the users, highlight current user.\nUsage: users\n\n")
+	fmt.Printf("agg: Aggregate the stored feeds at the provided interval/duration (ex: 1s, 1m, 1h15m, etc).\nUsage: agg <duration>\n\n")
+	fmt.Printf("addfeed: Add a new RSS feed.\nUsage: addfeed <name> <url>\n\n")
+	fmt.Printf("feeds: List feeds owned by logged in user\nUsage: feeds\n\n")
+	fmt.Printf("follow: Follow a feed (logged in user)\nUsage: follow <url>\n\n")
+	fmt.Printf("following: List feeds followed by logged in user \nUsage: following\n\n")
+	fmt.Printf("unfollow: Unfollow a feed (logged in user)\nUsage: unfollow <url>\n\n")
+	fmt.Printf("browse: List posts for logged in user. Default limit is 20.\nUsage: browse <limit>\n\n")
+	return nil
 }
